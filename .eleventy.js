587b6b9ffc76662f4837d7de4bd976089a9b0aff
Lmarcha38 <117238874+Lmarcha38@users.noreply.github.com>
2025-02-10 00:39:15 +0000

    Planet cards

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,12 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("card", function(name, description, color) {
+    return `<div class="card">
+      <span class="card_planet" style="background-color:${color}"></span>
+      <div class="card_name">${name}</div>
+      <div class="card_desc">${description}</div>
+    </div>`
+  })
 };
